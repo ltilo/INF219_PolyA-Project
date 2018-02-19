172c5dd30610be133581f17051ef9678dc073eaf
ltilo <florianisch.flo@googlemail.com>
2018-02-19 15:25:48 +0100

    New R script & Change Readme
    - New R script that shows what I did for finding overlaps and traceback
    - Changed the readme to give overview over files that exists

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ In my first step I focus on learning how to load up information from fast5
 files and plotting this. The rhdf5 library can also be used to manipulate
 the fast5 files. In this file you can see what I did to show the example data.
 
-Here some references and doc I used
-- (IONiseR) http://bioconductor.org/packages/release/bioc/html/IONiseR.html
-- (rhdf5) http://bioconductor.org/packages/release/bioc/html/rhdf5.html 
-
 ### using_poretools_and_minimap2_and_samtools.sh
 In this file you can see my steps how to create the *.bam file from the raw
 data. I used poretools, minimap2 aligner and samtools.
+
+### finding_overlap_and_traceback.R
+In this file I show what I did so far to finding overlaps. I also wrote a
+function that can be used for backtracing to the *.fast5 files
 
---
+++ finding_overlap_and_traceback.R
@@ -0,0 +1,61 @@
+library(GenomicAlignments)
+library(GenomicRanges)
+library(GenomicFeatures)
+
+#########################################################################################
+# Import *.bam file, *.gtf file and then find overlaps
+#########################################################################################
+
+# Reading stuff
+path_bam <- "path to bam"
+path_gtf <- "path to gtf"
+bam <- readGAlignments(path_bam, use.names = T) #Nanopore data *.bam Alignment
+gtf = makeTxDbFromGFF(path_gtf, format="gtf") # file is gtf location
+
+# Making reference transcripts
+transcripts <- transcriptsBy(gtf, "gene")
+# transcripts <- exonsBy(gtf, by="tx", use.names = T) # Making transcripts exon ???
+# transcripts <- transcriptsBy(gtf, "cds")
+# transcripts <- transcriptsBy(gtf, "exon")
+# transcripts <- extractTranscriptSeqs(gtf, use.names = T) # ??? is not working, no genome, how to import *.fa as genome?
+
+# Finding overlaps
+hits = findOverlaps(transcripts, bam) # hits object containing all hits
+# hits = countOverlaps(transcripts, foo) # Just an Integer array
+
+# Seperate all overlaps by name
+traceNames <- bam[to(hits)]@NAMES # alle foo har hits med transcripts
+
+# Other stuff that may be useful?
+transcripts[from(hits)] # alle transcripts som har hits med foo 
+
+
+#########################################################################################
+# Traceback to *.fast5 files that overlaps 
+#########################################################################################
+
+find_fast5_filename <- function(filepath_fastq, traceNames) {
+  t <- traceNames
+  f <- file(filepath_fastq, "r")
+  fast5_filenames <- vector(mode = "character", length = 0)
+  while ( TRUE ) {
+    line = readLines(f, n = 1)
+    if ( length(line) == 0 ) {
+      break
+    }
+    if(substring(line, 1, 1) == "@"){
+      # compute match between fastq file and trace list
+      line <- substring(line, 2)
+      splitLine <- unlist(strsplit(line, " "))
+      pos <- match(splitLine[1], t)
+      # Check if no match
+      if(is.na(pos)) next
+      # If match, add filename to file-list
+      fast5_filenames[length(fast5_filenames)+1] <- splitLine[2]
+      # Delete all entries for this match at the trace list for better performans
+      t <- t[! t %in% splitLine[1]]
+    }
+  }
+  close(f)
+  return(fast5_filenames)
+}
